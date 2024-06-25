626601698ecf0c2c3fcd388a631b7f7a8bf8e770
Nadezhda <nadezhdadieslast@gmail.com>
2024-06-25 18:03:39 +0400

    made a reverse array using pointers

--- main.cpp
+++ main.cpp
@@ -1,30 +1,28 @@
 #include <iostream>
 
-void reverseArray(int* array) {
-    int n = 10;
-    for (int i = 0; i < n / 2; ++i) {
-        int temp = array[i];
-        array[i] = array[n - 1 - i];
-        array[n - 1 - i] = temp;
+void reverseArray(int* array, int size) {
+    for (int i = 0; i < size / 2; i++) {
+        int temp = *(array + i);
+        *(array + i) = *(array + size - 1 - i);
+        *(array + size - 1 - i) = temp;
     }
 }
 
 int main() {
     int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int size = sizeof(arr) / sizeof(arr[0]);
 
     std::cout << "Original array: ";
-    for (int i : arr) {
+    for (int i: arr) {
         std::cout << i << " ";
     }
     std::cout << &arr << std::endl;
 
-    reverseArray(arr);
-
+    reverseArray(arr, size);
     std::cout << "Reverse array: ";
-    for (int i : arr) {
+    for (int i: arr) {
         std::cout << i << " ";
     }
     std::cout << &arr << std::endl;
-
-    return 0;
 }
+
